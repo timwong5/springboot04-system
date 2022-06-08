f29504c6ce339e5abb665f98ab865915af2c6a91
Yining Wang <wangyining203@gmail.com>
2022-06-08 23:36:11 +0800

    Initial commit

---
+++ src/main/java/com/timwang5/Springboot04SystemApplication.java
@@ -0,0 +1,13 @@
+package com.timwang5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot04SystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot04SystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/timwang5/Springboot04SystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.timwang5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot04SystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
